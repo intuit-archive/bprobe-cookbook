3219a8b76cd0fd425ed76dc91d959be66a326e24
Rick Mendes <rmendes@intuit.com>
2013-01-11 16:40:17 -0800

    replaced deprecated symbols with strings and added foodcritic

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'chef', '10.16.2'
+gem 'foodcritic', '1.7.0'
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Intuit CAP"
+maintainer_email "richard_mendes@intuit.com"
+license          "Apache 2.0"
+description      "Installs/Configures bprobe"
+long_description "Installs/Configures bprobe"
+version          "0.1.0"
+
+%w{ ubuntu centos amazon }.each do |os|
+  supports os
+end
+
+depends "yum"
+depends "apt"
